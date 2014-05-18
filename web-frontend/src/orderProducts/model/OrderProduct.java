89c6959f6704edf319b44d90b058d63789a8210f
Ashwin Ramesh <ashramesh1992@gmail.com>
2014-05-18 15:26:31 +1000

    making daos

--- web-frontend/src/orderProducts/model/OrderProduct.java
+++ web-frontend/src/orderProducts/model/OrderProduct.java
@@ -8,4 +8,44 @@ public class OrderProduct {
     private String description;
     private int quantity;
 
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
 }
