fcebaec0f642cff1d482f056523a5dfb3e0d5660
Ashwin Ramesh <ashramesh1992@gmail.com>
2014-05-18 18:00:51 +1000

    changes to schema and dao creation

--- sql/init.sql
+++ sql/init.sql
@@ -1,24 +1,28 @@
+DROP SCHEMA IF EXISTS `ecomsys`;
+CREATE SCHEMA IF NOT EXISTS `ecomsys` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE `ecomsys`;
 DROP TABLE if EXISTS `users`;
+DROP TABLE if EXISTS `user_roles`;
 DROP TABLE if EXISTS `orders`;
 DROP TABLE if EXISTS `order_products`;
 
 /* User table */
 CREATE TABLE users (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL, -- ensure unique
     password VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE user_roles (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY;
-    user_id INT(11) NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
     role VARCHAR(10) DEFAULT 'user',
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 /* Orders table */
 CREATE TABLE orders (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT(11) NOT NULL, -- make this a foreign key
     status ENUM('processing', 'shipping', 'delivered') DEFAULT 'processing',
     shipping_address_1 VARCHAR(255),
@@ -32,15 +36,18 @@ CREATE TABLE orders (
 
 /* Table that stores all products in each order */
 CREATE TABLE order_products (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
     product_id INT(11) NOT NULL,
     product_name VARCHAR(255) NOT NULL,
     description MEDIUMTEXT NOT NULL,
     quantity INT(4) DEFAULT 1,
-    order_id INT(11) NOT NULL,
+    order_id INT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE
 );
 
-INSERT INTO users (username, password, admin) VALUES ("userA", "passwordA", 0);
-INSERT INTO users (username, password, admin) VALUES ("userB", "passwordB", 0);
-INSERT INTO users (username, password, admin) VALUES ("admin", "admin", 1);
+INSERT INTO users (username, password) VALUES ("userA", "passwordA");
+INSERT INTO user_roles(user_id, role) VALUES (1, "user");
+INSERT INTO users (username, password) VALUES ("userB", "passwordB");
+INSERT INTO user_roles(user_id, role) VALUES (2, "user");
+INSERT INTO users (username, password) VALUES ("admin", "admin");
+INSERT INTO user_roles(user_id, role) VALUES (3, "admin");
--- web-frontend/src/orders/dao/OrderDAO.java
+++ web-frontend/src/orders/dao/OrderDAO.java
@@ -16,5 +16,115 @@ import javax.sql.DataSource;
 import orders.model.Order;
 
 public class OrderDAO {
+    private DataSource ds;
+    private String getOrdersByUser = "SELECT * FROM orders WHERE user_id = ?";
+    private String getAllOrders = "SELECT * FROM orders";
+    private String createOrder = "INSERT into orders (user_id, status, shipping_address_1, shipping_address_2, city, postcode, final_cost) VALUES (?,?,?,?,?,?,?)";
+    private String updateOrderStatus = "UPDATE orders SET status = ? WHERE id = ?";
+
+    public OrderDAO() throws Exception{
+        try{
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            // Look up our data source
+            ds = (DataSource) envCtx.lookup("jdbc/ShoppingCart");
+        }catch (NamingException e){
+            throw new Exception("cannot find database");
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public Boolean insertOrder(Order o) {
+        try{
+            Connection c = ds.getConnection();
+            PreparedStatement ps = c.prepareStatement(createOrder);
+            ps.setString(1, String.valueOf(o.getUserId()));
+            ps.setString(2, o.getStatus());
+            ps.setString(3, o.getShippingAddress1());
+            ps.setString(4, o.getShippingAddress2());
+            ps.setString(5, o.getCity());
+            ps.setString(6, o.getPostCode());
+            ps.setString(7, String.valueOf(o.getFinalCost()));
+            ps.executeUpdate();
+            ps.close();
+            c.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error in creating order");
+            return false;
+        }
+    }
+
+    public List<Order> getUserOrders(int userId) {
+        try{
+            Connection c = ds.getConnection();
+            PreparedStatement ps = c.prepareStatement(getOrdersByUser);
+            ps.setString(1, String.valueOf(userId));
+            ResultSet rs = ps.executeQuery();
+            List<Order> orders = new ArrayList<Order>();
+            while(rs.next()) {
+                orders.add(new Order(
+                    rs.getInt("user_id"),
+                    rs.getString("status"),
+                    rs.getString("shipping_address_1"),
+                    rs.getString("shipping_address_2"),
+                    rs.getString("city"),
+                    rs.getString("postcode"),
+                    rs.getDouble("final_cost")
+                ));
+            }
+            rs.close();
+            ps.close();
+            c.close();
+            return orders;
+        } catch (SQLException e) {
+            System.out.println("Error in getting user order");
+            return null;
+        }
+    }
+
+    public List<Order> getOrders() {
+        try{
+            Connection c = ds.getConnection();
+            PreparedStatement ps = c.prepareStatement(getAllOrders);
+            ResultSet rs = ps.executeQuery();
+            List<Order> orders = new ArrayList<Order>();
+            while(rs.next()) {
+                orders.add(new Order(
+                    rs.getInt("user_id"),
+                    rs.getString("status"),
+                    rs.getString("shipping_address_1"),
+                    rs.getString("shipping_address_2"),
+                    rs.getString("city"),
+                    rs.getString("postcode"),
+                    rs.getDouble("final_cost")
+                ));
+            }
+            rs.close();
+            ps.close();
+            c.close();
+            return orders;
+        } catch (SQLException e) {
+            System.out.println("Error in getting user order");
+            return null;
+        }
+    }
+
+    public Boolean updateStatusOrder(Order o) {
+        try{
+            Connection c = ds.getConnection();
+            PreparedStatement ps = c.prepareStatement(updateOrderStatus);
+            ps.setString(1, o.getStatus());
+            ps.setString(2, String.valueOf(o.getOrderId()));
+            ps.executeUpdate();
+            ps.close();
+            c.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error in updating order status");
+            return false;
+        }
+    }
 
 }
