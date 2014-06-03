6e1691cbe04a61b03336c14337dfe437da9e5802
Mojtaba <mujtaba.karami@gmail.com>
2014-06-03 18:02:33 +1000

    New branch

--- shipping-backend/src/Shipping.java
+++ shipping-backend/src/Shipping.java
@@ -4,8 +4,8 @@ import java.io.PrintWriter;
 
 
 // import javax.servlet.RequestDispatcher;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Shipping
  */
-@WebServlet("/Shipping")
+//@WebServlet("/Shipping")
 public class Shipping extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -25,10 +25,7 @@ public class Shipping extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	doPost(request, response);
-    }
-	/**
+  	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
